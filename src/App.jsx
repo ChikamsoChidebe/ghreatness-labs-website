21e341c718070111155e0f31d48638e30700fb66
Iyanu Oluwa <iyanu1396@gmail.com>
2024-12-20 11:18:02 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="flex items-center justify-center text-4xl font-bold text-red-600">
+        Welcome to Ghreatness Lab
+      </h1>
+    </div>
+  );
+}
+
+export default App;
